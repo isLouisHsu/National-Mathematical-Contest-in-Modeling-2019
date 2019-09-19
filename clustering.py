5861a7d1777e58814ac04b5f85362a5a0c257f04
louishsu <is.louishsu@foxmail.com>
2019-09-19 19:50:47 +0800

    Time: 2019-09-19 19:50:47

--- clustering.py
+++ clustering.py
@@ -6,7 +6,7 @@
 @Github: https://github.com/isLouisHsu
 @E-mail: is.louishsu@foxmail.com
 @Date: 2019-09-19 17:58:02
-@LastEditTime: 2019-09-19 19:13:33
+@LastEditTime: 2019-09-19 19:45:22
 @Update: 
 '''
 import os
@@ -16,20 +16,73 @@ import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.model_selection import GridSearchCV
 
 speedFeats = []
 for i in range(1, 4):
-    speedFeat = np.load('output/gpsSpeedFeat_file%d.npy')
+    speedFeat = np.load('output/gpsSpeedFeat_file%d.npy' % i)
     speedFeats += [speedFeat]
 speedFeats = np.concatenate(speedFeats, axis=0)
+n_samples, n_features = speedFeats.shape
 
-steps = [('scaler', StandardScaler()), ('pca', PCA(n_components=speedFeat.shape[1]))]
+# ------------------------------------------------------------------
+steps = [('scaler', StandardScaler()), ('pca', PCA(n_components=n_features))]
 pipline = Pipeline(steps)
-speedFeatsDecomposed = pipline.fit(speedFeats)
+pipline.fit(speedFeats)
 
-plt.figure()
+plt.figure(figsize=(8, 4))
 plt.title('Explained Variance(Eigen Values) Ratio of PCA')
-plt.plot(pipline['pca'].explained_variance_ratio_)
+plt.subplot(121)
+plt.plot(pipline.named_steps.pca.explained_variance_ratio_)
 plt.xlabel("Number of Components")
 plt.ylabel("Ratio of Explained Variance")
+plt.grid()
+plt.subplot(122)
+plt.plot([pipline.named_steps.pca.explained_variance_ratio_[:i+1].sum() for i in range(n_features)])
+plt.xlabel("Number of Components")
+plt.ylabel("Sum of Ratio of Explained Variance")
+plt.grid()
+plt.show()
+
+# ------------------------------------------------------------------
+# steps = [
+#         ('scaler', StandardScaler()), 
+#         ('pca', PCA()), 
+#         ('kmeans', KMeans()),
+#     ]
+# pipline = Pipeline(steps)
+
+# parameters = {
+#     'pca__n_components': [_ for _ in range(6, n_features)], 
+#     'kmeans__n_clusters': [_ for _ in range(3, 12)]
+#     }
+# searcher = GridSearchCV(pipline, parameters, cv=5)
+# searcher.fit(speedFeats)
+
+# print(searcher.cv_results_)
+# print(searcher.best_estimator_)
+# print(searcher.best_params_)
+
+# ------------------------------------------------------------------
+n_components = int(input("Please enter the number of components: "))
+steps = [
+        ('scaler', StandardScaler()), 
+        ('pca', PCA(n_components=n_components)), 
+        ('kmeans', KMeans()),
+    ]
+pipline = Pipeline(steps)
+
+scores = []
+for n_clusters in range(3, 15):
+    pipline.set_params(kmeans__n_clusters=n_clusters)
+    pipline.fit(speedFeats)
+    scores += [pipline.score(speedFeats)]
+
+plt.figure()
+plt.title("K-means, Scores")
+plt.plot(scores)
+plt.xlabel("Number of Clusters")
+plt.ylabel("Score")
+plt.grid()
 plt.show()
